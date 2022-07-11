b48c3f7a7027ad55ffb70d450e82033c754d52cb
nvvanh1 <nvvanh1@cmcglobal.vn>
2022-07-11 15:37:33 +0700

    backend

--- src/main/java/com/va/ServerVietanhShopApplication.java
+++ src/main/java/com/va/ServerVietanhShopApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServerVietanhShopApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServerVietanhShopApplication.class, args);
 	}
